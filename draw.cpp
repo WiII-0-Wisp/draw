ba8fce282e2bb5d3bd8440c56866810743aac4f2
WillOWisp <P.U.L_S.A.R@mail.ru>
2021-12-26 21:52:45 +0500

    Добавьте файлы проекта.

---
+++ draw.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+#include <fstream>
+
+int main() // по-моему комментарии излишни, тут всё очевидно
+{
+	using namespace std;
+	setlocale(0, "");
+	int N, m = 0;
+	cout << "Введите число бочонков в мешке: ";
+	ofstream outf("txt.txt", ios::app);
+	cin >> N;
+	outf << N << endl;
+	vector<bool> V(N, true);
+	cout << "Результаты жеребьёвки: ";
+	while (m < N) {
+		int x = rand() % N + 1;
+		if (V[x-1]) {
+			cout << x << " ";
+			outf << x << " ";
+			V[x - 1] = false;
+			++m;
+		}
+	}
+	outf << "\n\n";
+	outf.close();
+}
